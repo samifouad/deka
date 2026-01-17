f746ef81aec5cc08978ca1d6017ae6f748e3d009
Sami Fouad <sfouad@gmail.com>
2026-01-17 13:15:34 -0700

    checkpoint before starting introspect work

---
+++ crates/stdio/target/debug/deps/stdio-676000f3c4e383c1.d
@@ -0,0 +1,7 @@
+/Users/samifouad/Projects/deka/deka-stdio/target/debug/deps/stdio-676000f3c4e383c1.d: src/lib.rs src/terrace_font.rs src/terrace-font.flf
+
+/Users/samifouad/Projects/deka/deka-stdio/target/debug/deps/libstdio-676000f3c4e383c1.rmeta: src/lib.rs src/terrace_font.rs src/terrace-font.flf
+
+src/lib.rs:
+src/terrace_font.rs:
+src/terrace-font.flf:
